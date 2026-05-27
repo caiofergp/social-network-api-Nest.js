0895d265650b5d1175ac4ac237f013d03578b159
Caio Fernando <caiofergp@gmail.com>
2026-05-27 18:10:33 -0300

    fix feed module

--- src/posts/feed/feed.module.ts
+++ src/posts/feed/feed.module.ts
@@ -6,12 +6,15 @@ import { FeedController } from './feed.controller';
 import { FeedService } from './feed.service';
 import { AuthModule } from 'src/auth/auth.module';
 import { PrismaModule } from 'src/db/prisma/prisma.module';
+import { StorageAdapter } from 'src/adapters/storage/storage.adapter';
+import { MinionAdapter } from 'src/adapters/storage/minion.adapter';
 
 @Module({
   imports: [AuthModule, PrismaModule],
   providers: [
     FeedService,
     { provide: FeedRepository, useClass: PrismaFeedRepository },
+    { provide: StorageAdapter, useClass: MinionAdapter },
   ],
   controllers: [FeedController],
   exports: [FeedService],
